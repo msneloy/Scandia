c7b14c636c502533916d6945c660496f375831de
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-12 18:33:23 +0800

    calc CRC

--- C3&4/T6/src/app/home/home.page.ts
+++ C3&4/T6/src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
+  firstNumber;
+  secondNumber;
+  result;
   constructor() {}
+  add() {
+    this.result = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+  }
+  sub() {
+    this.result = parseInt(this.firstNumber) - parseInt(this.secondNumber);
+  }
 
+  mult() {
+    this.result = parseInt(this.firstNumber) * parseInt(this.secondNumber);
+  }
+
+  div() {
+    this.result = parseInt(this.firstNumber) / parseInt(this.secondNumber);
+  }
 }
