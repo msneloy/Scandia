f4a741274ca75503d6d11000a370942cec7ff9b3
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-20 07:41:53 +0800

    C5T2

--- C5/T2/src/app/home/home.page.ts
+++ C5/T2/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(public alertCtrl: AlertController) {}
 
-  constructor() {}
-
+  async showAlert() {
+    const alert = await this.alertCtrl.create({
+      header: "Sure to delete?",
+      message: "Are you sure?",
+      buttons: ["Delete", "Check for details", "Cancel"],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
+  }
 }
