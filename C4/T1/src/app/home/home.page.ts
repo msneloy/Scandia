1721b22dcc1785a224db0eddf3e0b3adffb4c450
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-19 21:39:01 +0800

    C4T1

--- C4/T1/src/app/home/home.page.ts
+++ C4/T1/src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
+  public gender: string;
   constructor() {}
-
+  genderyHandler(event) {
+    // get data throught event emitter
+    this.gender = event.target.value;
+  }
+  leavegender(event) {
+    console.log("bye bye ", event.target.value);
+  }
+  gogender(event) {
+    console.log("hello ", event.target.value);
+  }
 }
