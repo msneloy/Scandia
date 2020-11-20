1f5d920b60239e77ab64e432b91bf8131fc3446a
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-20 08:12:27 +0800

    C5T3

--- C5/T3/src/app/home/home.page.ts
+++ C5/T3/src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(public alertCtrl: AlertController) {}
 
-  constructor() {}
-
+  async showAlert() {
+    const alert = await this.alertCtrl.create({
+      header: "Exit this APP?",
+      message: "Are you sure?",
+      buttons: [
+        {
+          text: "Yes",
+          handler: () => {
+            this.exitApp();
+          },
+        },
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: (blah) => {
+            console.log("Confirm Cancel: blah");
+          },
+        },
+      ],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
+  }
 }
