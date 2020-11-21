1d81310d6bb708e6d05d841b0c9fe15f2d0b7c90
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-22 03:03:09 +0800

    C6

--- C6/T4/src/app/home/home.page.ts
+++ C6/T4/src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(public alertCtrl: AlertController) {}
+  async showPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: "User Login",
 
-  constructor() {}
-
+      inputs: [
+        {
+          name: "title",
+          type: "text",
+          placeholder: "Username",
+        },
+        {
+          name: "title",
+          type: "text",
+          placeholder: "passsword",
+        },
+      ],
+      buttons: [
+        {
+          text: "Login",
+          handler: (data) => {
+            console.log("Save clicked");
+          },
+        },
+        {
+          text: "Cancel",
+          handler: (data) => {
+            console.log("Cancel clicked");
+          },
+        },
+      ],
+    });
+    await prompt.present();
+  }
 }
