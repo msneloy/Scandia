c79edaa7ebe0f1eabd24ff755efac6b0654e3a6d
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-11-24 15:52:06 +0800

    C7T3

--- C7/T3/src/app/home/home.page.ts
+++ C7/T3/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor(public toastCtrl: ToastController) {}
+  async openToast1() {
+    const toast = await this.toastCtrl.create({
+      message: "Toast message prompt box displayed for a long time",
+      duration: 5000,
+    });
+    toast.present();
+  }
+  async openToast2() {
+    const toast = await this.toastCtrl.create({
+      message: "Toast message prompt box displayed for a short time",
+      duration: 1000,
+    });
+    toast.present();
+  }
 }
